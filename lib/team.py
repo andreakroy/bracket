8bdc13b85116d895827540670b8ef42b396a9808
Andrea Roy <akroy2@illinois.edu>
2021-01-01 12:04:22 -0800

    fixed team class

--- lib/team.py
+++ lib/team.py
@@ -1,25 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-class Team(object):
+class Team:
     def __init__(self, seed, name=None):
-        # seed - integer [1,16]
-        # region - str, used for debugging
+        # seed - integer [1, 16]
         # name - str
         self.seed = seed
-        # self.region = region
         self.name = name
 
-    def getSeed(self):
-        return self.seed
-##    def getRegion(self):
-##        return self.region
-    def getName(self):
-        return self.name
-    def getJSON(self):
-        return {"seed" : self.seed, "name" : self.name}
+    def __dict__(self):
+        return {'seed' : self.seed, 'name' : self.name}
+
     def __str__(self):
-        if self.name == None:
-            return str(self.seed) # + " from region " + str(self.region)
-        else:
-            return "(" + str(self.seed) + ") " + self.name
+        return f'[{self.seed}] ' + self.name
