ef87ab00fb0b664414239184dbd6f29511dd1bee
Andrea Roy <akroy2@illinois.edu>
2021-01-01 12:10:03 -0800

    added docs to team.py

--- lib/team.py
+++ lib/team.py
@@ -2,14 +2,29 @@
 # -*- coding: UTF-8 -*-
 
 class Team:
-    def __init__(self, seed, name=None):
-        # seed - integer [1, 16]
-        # name - str
+    '''
+    Defines a single team in the tournament.
+
+    Attributes
+    ----------
+    seed (int) - the team's tournament seed in the range [1, 16].
+    name (str) - the team's name
+    '''
+    def __init__(self, seed: int, name: str):
+        '''
+        Constructs a Team object
+        '''
         self.seed = seed
         self.name = name
 
     def __dict__(self):
+        '''
+        Returns a dict represenatiton of a Team object.
+        '''
         return {'seed' : self.seed, 'name' : self.name}
 
     def __str__(self):
+        '''
+        Returns a str representation of a Team object.
+        '''
         return f'[{self.seed}] ' + self.name
