e339fbb265e68bab278ec721ab8a987ceaefe914
Andrea Roy <akroy2@illinois.edu>
2021-01-01 12:16:40 -0800

    added more docs to team

--- lib/team.py
+++ lib/team.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+import csv
+import os
 
 class Team:
     '''
@@ -17,14 +19,32 @@ class Team:
         self.seed = seed
         self.name = name
 
-    def __dict__(self):
+    def __dict__(self) -> dict:
         '''
         Returns a dict represenatiton of a Team object.
         '''
         return {'seed' : self.seed, 'name' : self.name}
 
-    def __str__(self):
+    def __str__(self) -> str:
         '''
         Returns a str representation of a Team object.
         '''
         return f'[{self.seed}] ' + self.name
+
+path = os.path.dirname(os.path.realpath(__file__)) + "/"
+
+'''
+The file paths for the four regions with 16 teams each.
+Data files are csv of teams
+
+Specifications:
+First sixteen teams - South
+Second sixteen teams - East
+Third sixteen teams - Midwest
+Fourth sixteen teams - West
+Within each region: 1v16, 8v9, 5v12, 4v13, 6v11, 3v14, 7v10, 2v15
+'''
+files = [path + "data/data_UL.txt",
+         path + "data/data_LL.txt",
+         path + "data/data_LR.txt",
+         path + "data/data_UR.txt"]
