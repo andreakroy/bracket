ab50b8793cf0f701fc7204a3b44a93bae91e99c1
Kaahan <kaahan15@gmail.com>
2021-01-04 15:30:37 -0600

    F4A seeds generated randomly using numpy

--- lib/bracket/sample.py
+++ lib/bracket/sample.py
@@ -1,5 +1,6 @@
 from round import Rounds
 import random
+import numpy as np
 
 class Sample:
     '''
@@ -8,7 +9,7 @@ class Sample:
     def __init__(self):
         self.round_counts = { round: [] for round in Rounds }
         self.observed_counts = [57,29,17,13,7,3,3,5,1,1,4,0,0,0,0,0] # Observed counts of Final Four Appearances
-
+        self.pmf = self.calculate_pmf()
 
     '''
     Returns a probability from the probability mass function
@@ -19,7 +20,7 @@ class Sample:
     round (int) - the round for which the probability is desired.
     seed (int) - the seed for which the probability is desired.
     '''
-    def get_pmf(self) -> list:
+    def calculate_pmf(self) -> list:
         q = 0
         for seed in range(1, 17):
             q += (self.observed_counts[seed - 1] * seed)
@@ -35,10 +36,6 @@ class Sample:
         self.round_counts[Rounds.FINAL_4] = pmf
         return pmf
 
-    def run(self):
-        print(self.get_pmf())
- 
-
 class F4_A(Sample):
     '''
     Defines an F4_A sampling function; returns 4 seeds for the Final Four.
@@ -50,33 +47,21 @@ class F4_A(Sample):
     '''
     def __init__(self):
         Sample.__init__(self)
-        self.get_pmf()
         self.pmf = self.round_counts[Rounds.FINAL_4]
-        self.cdf = []
+        self.seeds = []
 
-    def get_seeds(self):
-        seeds = []
-        total = 0
+        rng = np.random.default_rng()
+        self.seeds = rng.choice(16, 4, p=self.pmf)
+        for i in range(len(self.seeds)):
+            self.seeds[i] += 1
 
-        for i in range(len(self.pmf)):
-            total += self.pmf[i]
-            self.cdf.append(total)
-            
-
-        for i in range(4):
-            r = random.random()
-            absolute_difference_function = lambda list_value : abs(list_value - r)
-
-            closest_value = min(self.pmf, key=absolute_difference_function)
-            ind = self.pmf.index(closest_value)
-            seeds.append(ind + 1)
-        return seeds
 
 if __name__ == "__main__":
     #y = Sample()
     #print(y.run())
     x = F4_A()
-    print(x.get_seeds())
+    #x.get_seeds()
+    print(x.pmf)
 '''
 class E_8(Sample):
     
