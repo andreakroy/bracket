911a077b190ef4875393ff155ac4449c3cccf96f
Andrea Roy <akroy2@illinois.edu>
2021-01-18 14:22:13 -0800

    cpanel file structure

---
+++ api/v1/app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from lib.bracket import Bracket, BracketType
+from lib.bracket.sample import F4_A, E_8
+from lib.database import getBracket
+from flask import Flask, request
+
+app = Flask(__name__)
+application = app
+
+@app.route('/', methods=['GET'])
+def generateJSON():  
+    args = request.args
+    if 'uid' in args:
+        uid = args['uid']
+        return getBracket(uid)
+        # TODO: check if the uid is in the database. If so return it.
+    else:
+        # Setting the men's bracket as the default since most users choose this.
+        bt = BracketType.MEN
+        if 'type' in args:
+            try:
+                bt = BracketType(args['type'])
+            except:
+                return 'Invalid type parameter', 422
+        sfn = None        
+        if 'sfn' in args:
+            if args['sfn'] == 'f4a':
+                sfn = F4_A()
+            elif args['sfn'] == 'e8':
+                sfn = E_8()
+        return Bracket(bt, sfn).to_json(), 200
+
+if __name__ == '__main__':
+    app.run()
---
+++ api/v1/lib/database.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from lib.bracket import Bracket, BracketType
+from lib.bracket.sample import Sample
+import os
+import json
+
+def getBracket(uid: int=None):
+    return 'bracket'
